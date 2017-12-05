b1c0ac9d86a66333877fc183b441414c91507b12
Hera Mentari <mail@heramentari.com>
2017-12-05 20:53:46 +0700

    Fixed login feature

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -28,6 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated()
 			.and()
 			.formLogin().loginPage("/login").permitAll()
+			.defaultSuccessUrl("/dashboard")
 			.and()
 			.logout().permitAll();
 	}
