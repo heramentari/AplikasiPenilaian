a88c5d3829a4b37b171289088a71512adb69fac4
Unknown <albertostevanus99@gmail.com>
2017-12-06 18:02:15 +0700

    Adding log for service

--- src/main/java/com/example/service/PenilaianServiceDatabase.java
+++ src/main/java/com/example/service/PenilaianServiceDatabase.java
@@ -22,37 +22,43 @@ public class PenilaianServiceDatabase implements PenilaianService
 
 	@Override
 	public UserModel selectUser(String username) {
+		log.info("Selected user with username {}", username);
 		return penilaianMapper.selectUser(username);
 	}
 	
 	@Override
 	public MataKuliahModel selectCourse(String kode_mk) {
+		log.info("Selected course with kode mk {}", kode_mk);
 		return penilaianMapper.selectCourse(kode_mk);
 	}
 	
 	@Override
 	public List<MataKuliahModel> selectCoursesByUser(String id) {
+		log.info("Selected course by user with id {}", id);
 		return penilaianMapper.selectCoursesByUser(id);
 	}
 	
 	@Override
 	public NilaiMkModel selectScore(String npm, String kode_mk) {
+		log.info("Selected score");
 		return penilaianMapper.selectScore(npm, kode_mk);
 	}
 
 	@Override
 	public StatistikNilaiMkModel lihatStatistikMatkul(String kode_mk) {
+		log.info("Selected statistic for course {}", kode_mk);
 		return penilaianMapper.lihatStatistikMatkul(kode_mk);
 	}
 
 	@Override
 	public StatistikNilaiMkModel selectNamaStatistikNilaiTertinggi(String kode_mk) {
+		log.info("Selected student with the highest score for course {}", kode_mk);
 		return penilaianMapper.selectNamaStatistikNilaiTertinggi(kode_mk);
 	}
 
 	@Override
 	public StatistikNilaiMkModel selectNamaStatistikNilaiTerendah(String kode_mk) {
+		log.info("Selected student with the lowest score for course {}", kode_mk);
 		return penilaianMapper.selectNamaStatistikNilaiTerendah(kode_mk);
 	}
-
 }
